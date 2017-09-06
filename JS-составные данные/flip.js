8e78af87fa7e4ba96d3019737acfb4d5c665e101
Pontarr <pontarr@gmail.com>
2017-09-06 18:28:53 +0300

    add pairs

--- JS-составные данные/flip.js
+++ JS-составные данные/flip.js
@@ -8,3 +8,16 @@ export default flip;
 // Example
 // const normalize = compose(Math.round, Math.abs);
 // const normalize = (value) => Math.round(Math.abs(value));
+
+
+//
+const cons = (a, b) => (message) => {
+switch (message)
+{
+  case 'car': return a;
+  case 'cdr': return b;
+}
+}
+
+const car = (pair) => pair('car');
+const cdr = (pair) => pair('cdr');
---
+++ JS-составные данные/pairs.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+export const cons = (x, y) => m => m(x, y);
+
+// BEGIN (write your solution here)
+export const car = z => z((x, y) => x);
+export const cdr = z => z((x, y) => y);
+// END
+
+var car = function car(z) {
+  return z(function (x, y) {
+    return x;
+  });
+};
